705aa43c7b50eaa4eb128aff6be1eeb19a90806c
PRO-teen <ayushyadav5867@gmail.com>
2026-07-31 10:53:35 +0530

    Revert "feat:add about section"
    This reverts commit 19b4da84eae62b9cce61f3ac09846a32212a67c8.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-// import "./App.css";
+import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import About from "./components/About";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
-      <About/>
     </>
   );
 }
